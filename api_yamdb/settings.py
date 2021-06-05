9fb070000dcc85b10744da669afa1638fd09d682
Andrey Kugubaev <andrey.kugubaev@yandex.ru>
2021-06-05 17:31:39 +0300

    fin6

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
