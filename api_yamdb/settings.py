050e127aa8bf73900b60a9cecc9fbd7ac105b9ba
Andrey Kugubaev <andrey.kugubaev@yandex.ru>
2021-06-06 13:06:22 +0300

    fin9

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -26,12 +26,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-    'testserver',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
